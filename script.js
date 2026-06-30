a8a693609009d22fa04d39f2766e6399eb81f3a3
Chandan Pandey <114169477+chandanp97@users.noreply.github.com>
2026-06-30 21:03:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,76 @@
+const typed = new Typed("#typing", {
+    strings: [
+      "AI Prompt Engineer",
+      "Data Analyst",
+      "Power BI Developer"
+    ],
+    typeSpeed: 70,
+    backSpeed: 45,
+    backDelay: 1500,
+    loop: true
+  });
+  
+  AOS.init({
+    duration: 1000,
+    once: true
+  });
+  const topBtn=document.getElementById("topBtn");
 
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.onclick=()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+};
+// ================================
+// Active Navbar
+// ================================
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
